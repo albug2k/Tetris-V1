76db189caf15179c328ddcfc4d327e7f817b1bee
albug2k <63882366+albug2k@users.noreply.github.com>
2023-11-19 20:44:19 -0500

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -36,6 +36,7 @@ data "aws_subnets" "public" {
 resource "aws_eks_cluster" "example" {
   name     = "EKS_CLOUD"
   role_arn = aws_iam_role.example.arn
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
   vpc_config {
     subnet_ids = data.aws_subnets.public.ids
